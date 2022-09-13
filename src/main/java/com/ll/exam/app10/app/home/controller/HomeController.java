c4676aba4fdc597be6b337d0bf6f55db8048d15d
nunusmohum <halojw301@gmail.com>
2022-09-13 13:14:08 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.ll.exam.app10.app.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @RequestMapping("/")
+    public String main() {
+        return "home/main";
+    }
+
+    @RequestMapping("/test/upload")
+    public String upload() {
+        return "home/test/upload";
+    }
+}
